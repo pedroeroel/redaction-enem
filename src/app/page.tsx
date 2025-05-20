bb0e8adec42ff8e3c9db9440ff9cccd2b8dc703e
pedroeroel <pedroedroel@gmail.com>
2025-05-20 11:09:35 -0300

    kokodaosdkaosd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,132 @@
-import Image from "next/image";
+'use client'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [result, setResult] = useState<string|null>(null);
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const essay = (form.elements.namedItem('essay') as HTMLTextAreaElement).value;
+    const essayTitle = (form.elements.namedItem('essayTitle') as HTMLInputElement).value;
+    if (!essay) {
+      alert('Please enter an essay');
+      return;
+    }
+    if (!essayTitle) {
+      alert('Please enter an essay title');
+      return;
+    }
+    if (essay.length < 400) {
+      alert('Essay must be at least 400 characters long');
+      return;
+    }
+    const response = await fetch('https://the-learners-dream.vercel.app/api/ENEM', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        "essay": essay,
+        "essayTitle": essayTitle
+      }
+      ),
+    });
+    const data = await response.json();
+    setResult(data);
+    console.log(data);
+    if (data.error) {
+      alert(data.error);
+      return;
+    } else  {
+      const resultContainer = document.querySelector('#result') as HTMLDivElement;
+
+      resultContainer.innerHTML = `
+        <h2 class="text-2xl font-semibold">Results:</h2>
+        <p class="text-lg">Your general score was: ${data.generalGrade}</p>
+        <p class="text-lg">Your essay was: ${data.comments[5]}</p>
+    
+        <ul class="list-disc pl-5">
+          <li class="text-lg">Competency 1: ${data.competencies[0]} - ${data.comments[0]}</li>
+          <li class="text-lg">Competency 2: ${data.competencies[1]} - ${data.comments[1]}</li>
+          <li class="text-lg">Competency 3: ${data.competencies[2]} - ${data.comments[2]}</li>
+          <li class="text-lg">Competency 4: ${data.competencies[3]} - ${data.comments[3]}</li>
+          <li class="text-lg">Competency 5: ${data.competencies[4]} - ${data.comments[4]}</li>
+        </ul>
+          `;
+      return;
+    }
+  }
+
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 sm:p-20">
+        <header className="flex w-full justify-center py-10 flex-col items-center gap-3">
+          <h1 className='text-4xl'>ENEM</h1>
+        <p className="text-lg text-center">
+          A simple tool to grade your essays for the ENEM exam
+        </p>
+        </header>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full max-w-xl">
+        <form
+          className="flex flex-col gap-4 w-full"
+          onSubmit={ handleSubmit }
+        >
+          <label htmlFor="essayTitle" className="font-semibold">Essay Title:</label>
+          <textarea
+            id="essayTitle"
+            name="essayTitle"
+            required
+            rows={1}
+            className="border rounded p-2 w-full"
+            placeholder="Cool essay about the future of AI..."
+          />
+          <label htmlFor="essay" className="font-semibold">Essay Content:</label>
+          <textarea
+            id="essay"
+            name="essay"
+            required
+            rows={10}
+            className="border rounded p-2 w-full"
+            placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis..."
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+            Assess
+          </button>
+        </form>
+
+        <section id='result'></section>
+
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://aistudio.google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          Gemini API
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://linkedin.com/in/pedroeroel/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          LinkedIn
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/pedroeroel"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Github
         </a>
       </footer>
-    </div>
+      </div>
+    
   );
 }
