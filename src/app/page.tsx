6e75275b7497ef72a89504436b4ffe24f5aebf69
pedroeroel <pedroedroel@gmail.com>
2025-05-20 12:42:38 -0300

    linkedin fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,7 +105,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://linkedin.com/in/pedroeroel/"
+          href="https://linkedin.com/in/pedroedroel/"
           target="_blank"
           rel="noopener noreferrer"
         >
